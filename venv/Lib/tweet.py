fdf1b4849df48bfd4447b2c39018cf28133dd8e5
rishiram435 <rishiram.subedi@gmail.com>
2019-11-15 08:28:30 +0545

    Authentication Module

--- venv/Lib/tweet.py
+++ venv/Lib/tweet.py
@@ -1,65 +1,18 @@
-#!/usr/bin/env python
-# encoding: utf-8
+# This apps will do the sentiment analysis of the recent tweets in my user account
 
-import tweepy  # https://github.com/tweepy/tweepy
-import csv
-
-# Twitter API credentials
-consumer_key = ""
-consumer_secret = ""
-access_key = ""
-access_secret = ""
+import tweepy
+from textblob import textblob
 
 
-def get_all_tweets(screen_name):
-    # Twitter only allows access to a users most recent 3240 tweets with this method
+consumerKey= 'dNLzQHqyAm6LgDotscnrOZm0Z'
 
-    # authorize twitter, initialize tweepy
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_key, access_secret)
-    api = tweepy.API(auth)
+consumerSecret='IOHhTZxkuU7T100VkpzxkXHaRyXLTAij42022WzqXaOll3qsXi '
 
-    # initialize a list to hold all the tweepy Tweets
-    alltweets = []
+accessToken= '1342197739-YcVWjU2FCknhZvVbZVKtG3RfnMWwNUCw3gyyM55 '
 
-    # make initial request for most recent tweets (200 is the maximum allowed count)
-    new_tweets = api.user_timeline(screen_name=screen_name, count=200)
+accessTokenSecret= 'z2Q6ZfRaURvgz2JqqY3B0hJVi6WBqY3fOkKPK6039DHEu'
 
-    # save most recent tweets
-    alltweets.extend(new_tweets)
+# Authentication module
+makeauth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 
-    # save the id of the oldest tweet less one
-    oldest = alltweets[-1].id - 1
-
-    # keep grabbing tweets until there are no tweets left to grab
-    while len(new_tweets) > 0:
-        print
-        "getting tweets before %s" % (oldest)
-
-        # all subsiquent requests use the max_id param to prevent duplicates
-        new_tweets = api.user_timeline(screen_name=screen_name, count=200, max_id=oldest)
-
-        # save most recent tweets
-        alltweets.extend(new_tweets)
-
-        # update the id of the oldest tweet less one
-        oldest = alltweets[-1].id - 1
-
-        print
-        "...%s tweets downloaded so far" % (len(alltweets))
-
-    # transform the tweepy tweets into a 2D array that will populate the csv
-    outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
-
-    # write the csv
-    with open('%s_tweets.csv' % screen_name, 'wb') as f:
-        writer = csv.writer(f)
-        writer.writerow(["id", "created_at", "text"])
-        writer.writerows(outtweets)
-
-    pass
-
-
-if __name__ == '__main__':
-    # pass in the username of the account you want to download
-    get_all_tweets("J_tsar")
+makeauth.set_access_token(accessToken,accessTokenSecret)
