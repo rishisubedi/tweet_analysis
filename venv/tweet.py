d635868942ff092910c64baf4a458afff896218e
rishiram435 <rishiram.subedi@gmail.com>
2019-11-15 08:51:27 +0545

    Authentication Module

---
+++ venv/tweet.py
@@ -0,0 +1,20 @@
+# This apps will do the sentiment analysis of the recent tweets in my user account
+
+import tweepy
+import textblob
+
+
+consumerKey= 'dNLzQHqyAm6LgDotscnrOZm0Z'
+
+consumerSecret='IOHhTZxkuU7T100VkpzxkXHaRyXLTAij42022WzqXaOll3qsXi '
+
+accessToken= '1342197739-YcVWjU2FCknhZvVbZVKtG3RfnMWwNUCw3gyyM55 '
+
+accessTokenSecret= 'z2Q6ZfRaURvgz2JqqY3B0hJVi6WBqY3fOkKPK6039DHEu'
+
+# Authentication module for acessing the twitter
+makeauth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+
+makeauth.set_access_token(accessToken,accessTokenSecret)
+
+api = tweepy.API(makeauth)
